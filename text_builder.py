13beb4a0fe3ca76ec70354e249b4d0bff6d40da3
Samuel Keullen <sk.caragua2017@gmail.com>
2022-04-02 20:34:00 -0300

    Text Builder Script(RNN) added.

--- text_builder.py
+++ text_builder.py
@@ -1,105 +1,277 @@
 import tensorflow as tf
+
 import numpy as np
 import os
-import pickle
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
-from string import punctuation
-import requests
+import time
 
-#We are going to use a free downloadable book as the dataset for this tutorial: Alice’s Adventures in Wonderland by Lewis Carroll. But you can use any book/corpus you want.
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
-content = requests.get("http://www.gutenberg.org/cache/epub/11/pg11.txt").text
-open("data/wonderland.txt", "w", encoding="utf-8").write(content)
+# Read, then decode for py2 compat.
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+# length of text is the number of characters in it
+print(f'Length of text: {len(text)} characters')
 
-sequence_length = 100
-BATCH_SIZE = 128
-EPOCHS = 30
-# dataset file path
-FILE_PATH = "data/wonderland.txt"
-BASENAME = os.path.basename(FILE_PATH)
-# read the data
-text = open(FILE_PATH, encoding="utf-8").read()
-# remove caps, comment this code if you want uppercase characters as well
-text = text.lower()
-# remove punctuation
-text = text.translate(str.maketrans("", "", punctuation))
+print("\n")
 
-# print some stats
-n_chars = len(text)
-vocab = ''.join(sorted(set(text)))
-print("unique_chars:", vocab)
-n_unique_chars = len(vocab)
-print("Number of characters:", n_chars)
-print("Number of unique characters:", n_unique_chars)
+# Take a look at the first 250 characters in text
+print(text[:250])
 
-# dictionary that converts characters to integers
-char2int = {c: i for i, c in enumerate(vocab)}
-# dictionary that converts integers to characters
-int2char = {i: c for i, c in enumerate(vocab)}
+print("\n")
 
-# save these dictionaries for later generation
-pickle.dump(char2int, open(f"{BASENAME}-char2int.pickle", "wb"))
-pickle.dump(int2char, open(f"{BASENAME}-int2char.pickle", "wb"))
+# The unique characters in the file
+vocab = sorted(set(text))
+print(f'{len(vocab)} unique characters')
 
-# convert all text into integers
-encoded_text = np.array([char2int[c] for c in text])
+print("\n")
 
-# construct tf.data.Dataset object
-char_dataset = tf.data.Dataset.from_tensor_slices(encoded_text)
+example_texts = ['abcdefg', 'xyz']
 
-# print first 5 characters
-for char in char_dataset.take(8):
-    print(char.numpy(), int2char[char.numpy()])
+chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+print(chars)
 
-# build sequences by batching
-sequences = char_dataset.batch(2*sequence_length + 1, drop_remainder=True)
+print("\n")
 
-# print sequences
-for sequence in sequences.take(2):
-    print(''.join([int2char[i] for i in sequence.numpy()]))
+ids_from_chars = tf.keras.layers.StringLookup(
+    vocabulary=list(vocab), mask_token=None)
 
-# build sequences by batching
-sequences = char_dataset.batch(2*sequence_length + 1, drop_remainder=True)
+ids = ids_from_chars(chars)
+print(ids)
 
-# print sequences
-for sequence in sequences.take(2):
-    print(''.join([int2char[i] for i in sequence.numpy()]))
+print("\n")
 
-def split_sample(sample):
-    # example :
-    # sequence_length is 10
-    # sample is "python is a great pro" (21 length)
-    # ds will equal to ('python is ', 'a') encoded as integers
-    ds = tf.data.Dataset.from_tensors((sample[:sequence_length], sample[sequence_length]))
-    for i in range(1, (len(sample)-1) // 2):
-        # first (input_, target) will be ('ython is a', ' ')
-        # second (input_, target) will be ('thon is a ', 'g')
-        # third (input_, target) will be ('hon is a g', 'r')
-        # and so on
-        input_ = sample[i: i+sequence_length]
-        target = sample[i+sequence_length]
-        # extend the dataset with these samples by concatenate() method
-        other_ds = tf.data.Dataset.from_tensors((input_, target))
-        ds = ds.concatenate(other_ds)
-    return ds
 
-split_sample("python is a great pro")
+chars_from_ids = tf.keras.layers.StringLookup(
+    vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
 
-# prepare inputs and targets
-dataset = sequences.flat_map(split_sample)
+chars = chars_from_ids(ids)
+print(chars)
 
-def one_hot_samples(input_, target):
-    # onehot encode the inputs and the targets
-    # Example:
-    # if character 'd' is encoded as 3 and n_unique_chars = 5
-    # result should be the vector: [0, 0, 0, 1, 0], since 'd' is the 4th character
-    return tf.one_hot(input_, n_unique_chars), tf.one_hot(target, n_unique_chars)
+print("\n")
 
-# print first 2 samples
-for element in dataset.take(2):
-    print("Input:", ''.join([int2char[np.argmax(char_vector)] for char_vector in element[0].numpy()]))
-    print("Target:", int2char[np.argmax(element[1].numpy())])
-    print("Input shape:", element[0].shape)
-    print("Target shape:", element[1].shape)
-    print("="*50, "\n")
+tf.strings.reduce_join(chars, axis=-1).numpy()
+
+def text_from_ids(ids):
+  return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+
+all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+print(all_ids)
+
+print("\n")
+
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+
+for ids in ids_dataset.take(10):
+    print(chars_from_ids(ids).numpy().decode('utf-8'))
+
+print("\n")
+
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
+
+sequences = ids_dataset.batch(seq_length+1, drop_remainder=True)
+
+for seq in sequences.take(1):
+  print(chars_from_ids(seq))
+
+print("\n")
+
+
+for seq in sequences.take(5):
+  print(text_from_ids(seq).numpy())
+
+print("\n")
+
+def split_input_target(sequence):
+    input_text = sequence[:-1]
+    target_text = sequence[1:]
+    return input_text, target_text
+
+split_input_target(list("Tensorflow"))
+
+dataset = sequences.map(split_input_target)
+
+for input_example, target_example in dataset.take(1):
+    print("Input :", text_from_ids(input_example).numpy())
+    print("Target:", text_from_ids(target_example).numpy())
+
+print("\n")
+
+print("Let's setup the dataset...")
+
+# Batch size
+BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = (
+    dataset
+    .shuffle(BUFFER_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True)
+    .prefetch(tf.data.experimental.AUTOTUNE))
+
+print(dataset)
+
+# Length of the vocabulary in chars
+vocab_size = len(vocab)
+
+# The embedding dimension
+embedding_dim = 256
+
+# Number of RNN units
+rnn_units = 1024
+
+print("\nnow we'll create the model\n")
+
+class MyModel(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, rnn_units):
+    super().__init__(self)
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(rnn_units,
+                                   return_sequences=True,
+                                   return_state=True)
+    self.dense = tf.keras.layers.Dense(vocab_size)
+
+  def call(self, inputs, states=None, return_state=False, training=False):
+    x = inputs
+    x = self.embedding(x, training=training)
+    if states is None:
+      states = self.gru.get_initial_state(x)
+    x, states = self.gru(x, initial_state=states, training=training)
+    x = self.dense(x, training=training)
+
+    if return_state:
+      return x, states
+    else:
+      return x
+
+print("Creating an object of our model...\n")
+
+model = MyModel(
+    # Be sure the vocabulary size matches the `StringLookup` layers.
+    vocab_size=len(ids_from_chars.get_vocabulary()),
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units)
+
+for input_example_batch, target_example_batch in dataset.take(1):
+    example_batch_predictions = model(input_example_batch)
+    print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+
+print("\n")
+
+model.summary()
+
+sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
+
+print(sampled_indices)
+
+print("\n")
+
+print("Input:\n", text_from_ids(input_example_batch[0]).numpy())
+print()
+print("Next Char Predictions:\n", text_from_ids(sampled_indices).numpy())
+
+print("\nNow we'll finally train the model...")
+
+print("\n")
+
+loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+example_batch_mean_loss = loss(target_example_batch, example_batch_predictions)
+print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
+print("Mean loss:        ", example_batch_mean_loss)
+
+tf.exp(example_batch_mean_loss).numpy()
+
+model.compile(optimizer='adam', loss=loss)
+
+# Directory where the checkpoints will be saved
+checkpoint_dir = './results'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+print("Let's start the training\n")
+
+EPOCHS = 20
+
+history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+
+class OneStep(tf.keras.Model):
+  def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+    super().__init__()
+    self.temperature = temperature
+    self.model = model
+    self.chars_from_ids = chars_from_ids
+    self.ids_from_chars = ids_from_chars
+
+    # Create a mask to prevent "[UNK]" from being generated.
+    skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+    sparse_mask = tf.SparseTensor(
+        # Put a -inf at each bad index.
+        values=[-float('inf')]*len(skip_ids),
+        indices=skip_ids,
+        # Match the shape to the vocabulary
+        dense_shape=[len(ids_from_chars.get_vocabulary())])
+    self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+
+  @tf.function
+  def generate_one_step(self, inputs, states=None):
+    # Convert strings to token IDs.
+    input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+    input_ids = self.ids_from_chars(input_chars).to_tensor()
+
+    # Run the model.
+    # predicted_logits.shape is [batch, char, next_char_logits]
+    predicted_logits, states = self.model(inputs=input_ids, states=states,
+                                          return_state=True)
+    # Only use the last prediction.
+    predicted_logits = predicted_logits[:, -1, :]
+    predicted_logits = predicted_logits/self.temperature
+    # Apply the prediction mask: prevent "[UNK]" from being generated.
+    predicted_logits = predicted_logits + self.prediction_mask
+
+    # Sample the output logits to generate token IDs.
+    predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+    predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+
+    # Convert from token ids to characters
+    predicted_chars = self.chars_from_ids(predicted_ids)
+
+    # Return the characters and model state.
+    return predicted_chars, states
+
+one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+start = time.time()
+states = None
+next_char = tf.constant(['ROMEO:'])
+result = [next_char]
+
+for n in range(1000):
+  next_char, states = one_step_model.generate_one_step(next_char, states=states)
+  result.append(next_char)
+
+result = tf.strings.join(result)
+end = time.time()
+print(result[0].numpy().decode('utf-8'), '\n\n' + '_'*80)
+print('\nRun time:', end - start)
+
+tf.saved_model.save(one_step_model, 'one_step')
+one_step_reloaded = tf.saved_model.load('one_step')
+
+states = None
+next_char = tf.constant(['ROMEO:'])
+result = [next_char]
+
+for n in range(100):
+  next_char, states = one_step_reloaded.generate_one_step(next_char, states=states)
+  result.append(next_char)
+
+print(tf.strings.join(result)[0].numpy().decode("utf-8"))
